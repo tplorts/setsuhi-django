418232b95d3c464c5c44f7a78447c501b32ec7d4
Ted Lorts <tplorts@gmail.com>
2014-01-28 16:51:54 +0900

    custom button hover :)

---
+++ home/static/main/scripts/buttons.js
@@ -0,0 +1,73 @@
+
+EXPAND = 1, CONTRACT = -1;
+LAST_STEP = 5;
+
+function HoverHandler( jqButton ) {
+    this.button = jqButton;
+    this.direction = EXPAND;
+    this.stepIndex = 0;
+    this.intervalId = null;
+}
+
+HoverHandler.prototype.presentClass = function() {
+    return "expand-step-" + this.stepIndex;
+}
+
+HoverHandler.prototype.stop = function() {
+    if( this.intervalId ) {
+        clearInterval( this.intervalId );
+        this.intervalId = null;
+    }
+}
+
+HoverHandler.prototype.step = function() {
+    this.button.removeClass( this.presentClass() );
+    this.stepIndex += this.direction;
+    if( this.stepIndex <= 0 ) {
+        this.stop();
+        this.stepIndex = 0;
+    } else if( this.stepIndex >= LAST_STEP ) {
+        this.stop();
+        this.stepIndex = LAST_STEP;
+    }
+    this.button.addClass( this.presentClass() );
+}
+
+HoverHandler.prototype.start = function( newDirection ) {
+    this.stop();
+    this.direction = newDirection;
+    var self = this;
+    this.intervalId = setInterval( function(){self.step();}, 40 );
+}
+
+HoverHandler.prototype.expand = function() {
+    this.start( EXPAND );
+}
+
+HoverHandler.prototype.contract = function() {
+    this.start( CONTRACT );
+}
+
+
+
+hoverRegistry = [];
+
+function hoverHandler( button ) {
+    bid = button.attr("id");
+    if( !(bid in hoverRegistry) )
+        hoverRegistry[bid] = new HoverHandler( button );
+    return hoverRegistry[bid];
+}
+
+
+
+enter = function(event) {
+    hoverHandler( $(this) ).expand();
+}
+
+exit = function(event) {
+    hoverHandler( $(this) ).contract();
+}
+
+$(".navigation-button").hover( enter, exit );
+
