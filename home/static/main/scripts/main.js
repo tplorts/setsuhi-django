4500b4a431a8dece8d692679e0e4b951990dab00
Ted Lorts <tplorts@gmail.com>
2014-02-05 11:00:07 +0900

    left dock added with setsuhi title for example

---
+++ home/static/main/scripts/main.js
@@ -0,0 +1,11 @@
+$(".navigation-button").smoothHover();
+
+(function( $ ){
+    $.fn.insertBreaks = function() {
+        moji = $(this).text().trim().split('');
+        $(this).html( moji.join("<br>") );
+    }; 
+})( jQuery );
+
+
+$(".vertical-text").insertBreaks();
