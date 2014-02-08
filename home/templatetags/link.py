72d6dd065eeae24548d7bd68ea87628eec298362
Ted Lorts <tplorts@gmail.com>
2014-02-08 19:25:52 +0900

    auto insert lang of link name

--- home/templatetags/link.py
+++ home/templatetags/link.py
@@ -10,25 +10,30 @@ en_url = "http://ja.wikipedia.org/wiki/%E7%B8%81"
 sakaiki_url = "http://sakaiki.modalbeats.com/"
 jiang_url = "http://jiang-xiaoqing.xii.jp/"
 
-people = {
+# If the display name is in Roman characters, I want the
+# English font used.  Even if the link appears in a
+# Japanese section.
+english_addresses = {
     "Jay Anderson": "http://www.jayandersonbass.com/",
     "Frank Kinbrough": "http://home.earthlink.net/~fkimbrough/",
     "Adam Unsworth": "http://adamunsworth.net/",
     "London Improvisers Orchestra": "http://www.londonimprovisersorchestra.co.uk/",
     "Vasco da Gama": "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Ddigital-text&field-keywords=magellan+japan+vasco+da+gama",
     "Jiang Xiao-Qing": jiang_url,
-    "姜小青":  jiang_url,
     "Fissler": "http://www.fissler.com/",
     "RYONA": "http://ryona.jp",
+    "Sakaiki": sakaiki_url,
+    "Saideigama": "http://saideigama.com/index_eng.html",
+#    "縁 (&ldquo;en&rdquo;)": en_url,
+};
+japanese_addresses = {
+    "姜小青":  jiang_url,
     "喫茶茶会記": sakaiki_url,
     "茶会記": sakaiki_url,
-    "Sakaiki": sakaiki_url,
     "日本代表新ユニフォーム": "http://adidas.jp/jfa/",
-    "Saideigama": "http://saideigama.com/index_eng.html",
     "彩泥窯": "http://saideigama.com/taiken_tepure_syodo_saport.html",
 #    "縁": en_url,
 #    "〜en〜縁": en_url,
-#    "縁 (&ldquo;en&rdquo;)": en_url,
 };
 
 
@@ -45,8 +50,14 @@ def link( name, lang=None ):
 #    else:
 #        url = people[name]
 #    return { 'name': name, 'url': url, 'lang': lang }
-    if( name not in people ):
+    
+    if( name in english_addresses ):
+        lang = "en"
+    elif( name in japanese_addresses ):
+        lang = "ja"
+    else:
         return name
+
     a = "<a "
     target = "target=\"_blank\" "
     href = "href=\"" + people[name] + "\" "
