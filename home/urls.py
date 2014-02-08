6aefb3ac1ad5c075de9bd1c053a8db887b87bd6c
Ted Lorts <tplorts@gmail.com>
2014-02-09 01:25:42 +0900

    I think we'll call the works page seisaku

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from home import views
 urlpatterns = patterns('',
     url(r'^$', views.front, name='front'),
     url(r'^about/', views.about, name='about'),
-# mada..    url(r'^artwork/', views.artwork, name='artwork'),
+# mada..    url(r'^works/', views.works, name='works'),
     url(r'^media/', views.media, name='media'),
     url(r'^lessons/', views.lessons, name='lessons'),
     url(r'^contact/', views.contact, name='contact'),
