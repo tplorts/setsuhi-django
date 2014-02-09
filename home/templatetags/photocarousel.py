2715ae3f3ef82b53c3a220a52b4cc4f6c6e7c4e7
Ted Lorts <tplorts@gmail.com>
2014-02-09 19:06:34 +0900

    loop to generate slideshow now

--- home/templatetags/photocarousel.py
+++ home/templatetags/photocarousel.py
@@ -3,7 +3,9 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('tags/photocarousel.html', takes_context=True)
-def photocarousel( context, carousel_id, photo_path ):
+def photocarousel( context, carousel_id, photo_path, quantity ):
+    quantity = int( quantity )
     context["carousel_id"] = carousel_id
     context["photo_path"] = photo_path
+    context["photos"] = [str(n)+".jpg" for n in range(0, quantity)]
     return context
