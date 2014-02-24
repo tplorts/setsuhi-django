b595f01295732bc818c5244a63fa7bef17b50f77
Ted Lorts <tplorts@gmail.com>
2014-02-24 18:05:52 +0900

    loop for nav

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render
 
 ML_COOKIE_NAME = "ml-language-selection"
 ML_CONTEXT_KEY = "ml_active_language"
+
+nav_list = (
+    ("about", "About", "紹介"),
+    ("media", "Media", "画像"),
+    ("lessons", "Lessons", "教室"),
+    ("contact", "Contact", "連絡"),    
+)
 
 def ml_selection(request):
     if ML_COOKIE_NAME in request.COOKIES:
@@ -12,6 +21,7 @@ def ml_selection(request):
 
 
 def view_with_ml(request, view_name, context={}):
+    context["nav_list"] = nav_list
     context[ML_CONTEXT_KEY] = ml_selection(request)
     context['present_view_name'] = view_name.split("/")[-1]
     return render(request, 'main/'+view_name+'.html', context)
