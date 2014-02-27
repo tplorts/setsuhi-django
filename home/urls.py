273ddc54ed313c37e28c2f53b23f9a037a5225a7
Ted Lorts <tplorts@gmail.com>
2014-02-27 12:10:13 +0900

    fixing redirect to icon file

--- home/urls.py
+++ home/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import patterns, url
 from django.views.generic.base import RedirectView
 
 from home import views
+from setsuhi import settings
+
+s3 = settings.S3_STATIC_URL
 
 urlpatterns = patterns('',
 
     # Place the favicon in a standard static location but still conform to an old fashion favicon
-    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/main/icons/favicon.ico'), name='favicon'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url=s3+'main/icons/favicon.ico'), name='favicon'),
                        
     url(r'^$', views.front, name='front'),
     url(r'^about/', views.about, name='about'),
