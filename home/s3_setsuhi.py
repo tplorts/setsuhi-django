3f0c257a1e7d960931401e2a54df148ab46eacfd
Ted Lorts <tplorts@gmail.com>
2014-03-03 16:24:44 +0900

    had to swap setsuhi credentials because the github repo was public

--- home/s3_setsuhi.py
+++ home/s3_setsuhi.py
@@ -2,7 +2,7 @@ from boto.s3.connection import S3Connection
 
 
 # These are the credentials for the user 'setsuhi'
-cxn = S3Connection('AKIAIQDCJ3TJSVSEJHUQ', '7+zUUT2oiIx6R0KAzQPNSVbBJAUnN99sMB4fxUR4')
+cxn = S3Connection('AKIAJZDM7UFOXEZXO4NA', 'bAK78NsqgSvGekyfyjYuI2rJo6jTrZPbhBGoOglk')
 
 bucket = cxn.get_bucket('setsuhi-tokyo')
 
