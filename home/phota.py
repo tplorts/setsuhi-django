a561847d683ec95d121d0865001c8eac16ec216e
Ted Lorts <tplorts@gmail.com>
2014-03-03 23:22:30 +0900

    list the folders in the s3 photum place

--- home/phota.py
+++ home/phota.py
@@ -1,8 +1,10 @@
+from __future__ import unicode_literals
 import s3_setsuhi
+import urllib
 
 
-names = (
-    "2013-7-19 Setsuhi+Miya",
-    "2013-8-23 Fissler",
-)
+ls = s3_setsuhi.bucket.list(prefix="photographs/", delimiter="/")
 
+names = [urllib.unquote_plus(x.name.split('/')[1]) for x in ls]
+# The first object is the key of the query
+names = names[1:]
