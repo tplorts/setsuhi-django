27812d36398374c9d524bb2b6463ef5343845673
Ted Lorts <tplorts@gmail.com>
2014-03-12 14:55:18 +0900

    needed to more specifically target links intended for page scrolling

--- home/static/main/scripts/main.js
+++ home/static/main/scripts/main.js
@@ -13,7 +13,7 @@ $(".vertical-text").insertBreaks();
 //=================================================================
 // Source: http://css-tricks.com/snippets/jquery/smooth-scrolling/
 $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
+    $('a.smooth-page-scroll[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
