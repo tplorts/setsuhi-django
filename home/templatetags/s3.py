cc395e54766f5c38035d33e69978985068602859
Ted Lorts <tplorts@gmail.com>
2014-03-18 20:50:21 +0900

    s3 tags and no more open sans

--- home/templatetags/s3.py
+++ home/templatetags/s3.py
@@ -5,6 +5,17 @@ from home import s3_setsuhi
 register = template.Library()
 
 
+@register.simple_tag
+def s3( key ):
+    return s3_setsuhi.bucket_url + key
+
+
+@register.assignment_tag
+def s3list( folder ):
+    ls = s3_setsuhi.bucket.list( prefix=folder, encoding_type="url" )
+    return [ item for item in ls ][1:]
+
+
 @register.inclusion_tag('tags/s3photoset.html')
 def s3photoset( place ):
     ls = s3_setsuhi.bucket.list(prefix="photographs/"+place, encoding_type="url")
