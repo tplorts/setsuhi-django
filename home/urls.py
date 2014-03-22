3c085e3a00767dd158185357479345cf08410b0c
Ted Lorts <tplorts@gmail.com>
2014-03-22 11:51:35 +0900

    women's experience redirect url

--- home/urls.py
+++ home/urls.py
@@ -6,12 +6,19 @@ from setsuhi import settings
 
 s3 = settings.S3_STATIC_URL
 
+
+woman = RedirectView.as_view(url="http://restaurants.tokyo.grand.hyatt.co.jp/french-kitchen-tokyo/news/woman-experience.html")
+
+
 urlpatterns = patterns(
     '',
 
     # Place the favicon in a standard static location but still conform to an old fashion favicon
     url(r'^favicon\.ico$', RedirectView.as_view(url=s3+'main/icons/favicon.ico'), name='favicon'),
-                       
+
+    # For readability I listed this url on a flyer
+    url(r'^wom[ae]n(s?)(-?)experience$', woman),
+
     url(r'^$', views.front, name='front'),
     url(r'^about/', views.about, name='about'),
     url(r'^works/$', views.works, name='works'),
