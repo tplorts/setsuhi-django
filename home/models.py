5e8408534d20249967863340ff58b34040700fe9
Ted Lorts <tplorts@gmail.com>
2014-04-01 11:34:02 +0900

    initial Sakuhin model including the group idea

--- home/models.py
+++ home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+
+class SakuhinGroup( models.Model ):
+    name = models.CharField( max_length=100 )
+
+    def __unicode__( self ):
+        return self.name
+
+
+class Sakuhin( models.Model ):
+    main_image_url = models.URLField()
+    thumb_image_url = models.URLField( blank=True )
+    large_image_url = models.URLField( blank=True )
+    title = models.CharField( max_length=60, blank=True )
+    brief = models.CharField( max_length=200, blank=True )
+    lengthy = models.TextField( blank=True )
+    is_for_sale = models.BooleanField( default=False )
+
+    groups = models.ManyToManyField( SakuhinGroup )
+
+    def __unicode__( self ):
+        return self.title
+
