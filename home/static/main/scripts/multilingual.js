86cbd2da6bcd7a88efecd2fb9ee47000644e7a7d
Ted Lorts <tplorts@gmail.com>
2014-04-08 07:31:21 +0900

    refactoring Multilingual

--- home/static/main/scripts/multilingual.js
+++ home/static/main/scripts/multilingual.js
@@ -1,4 +1,3 @@
-mlWidget = $(".ml-widget");
 
 (function( $ ){
    $.fn.mlFindLocal = function( languageCode ) {
@@ -6,77 +5,119 @@ mlWidget = $(".ml-widget");
    }; 
 })( jQuery );
 
-
-function mlButton( lang ) {
-    return mlWidget.find( ".ml-language[lang='" + lang + "']" );
+function Multilingual() {
+    this.COOKIE_KEY = "ml-language-selection";
+    this.widgets = null;
+    this.activeLanguage = null;
 }
 
+Multilingual.prototype.cookied = function() {
+    return $.cookie(this.COOKIE_KEY);
+}
 
-function setTitleLanguage( languageCode ) {
+Multilingual.prototype.cookie = function( lang ) {
+    $.removeCookie(this.COOKIE_KEY);
+    $.cookie(this.COOKIE_KEY, lang, {path: '/', expires: 3650});    
+}
+
+Multilingual.prototype.button = function( lang ) {
+    return this.widgets.find( ".ml-language[lang='" + lang + "']" );
+}
+
+Multilingual.prototype.applyTitle = function( languageCode ) {
     titles = $("#ml-titles .ml");
-    if( titles ) {
-        localTitleElement = titles.mlFindLocal( languageCode );
-        if( localTitleElement ) {
-            document.title = localTitleElement.text();
+    if( titles && titles.length > 0 ) {
+        local = titles.mlFindLocal( languageCode );
+        if( local && local.length > 0 ) {
+            document.title = local.text();
         }
     }
 }
 
 
-function indicateActive( langButton ) {
-    mlWidget.find(".ml-language.active").removeClass("active");
+Multilingual.prototype.indicateActiveButton = function( langButton ) {
+    this.widgets.find(".ml-language.active").removeClass("active");
     langButton.addClass("active");
 }
 
 
-function setLanguage( lang, animated ) {
-    langButton = mlButton( lang );
+Multilingual.prototype.activate = function( lang, animated ) {
+    langButton = this.button( lang );
+
+    // Check that the button for the new language exists,
+    // implicitly meaning that the language is available.
     if( !langButton ) return;
 
-    setTitleLanguage(lang);
-    indicateActive( langButton );
+    this.activeLanguage = lang;
 
-    $.removeCookie("ml-language-selection");
-    $.cookie("ml-language-selection", lang, {path: '/', expires: 3650});
+    // Apply this language's title and visually change the buttons.
+    this.applyTitle( lang );
+    this.indicateActiveButton( langButton );
 
-    ml_elements = $(".ml");
-    former = ml_elements.find(".ml-on");
-    latter = ml_elements.mlFindLocal( lang );
+    // Update the cookie
+    this.cookie( lang );
 
-    if( former && latter ) {
+    // Update all multilingual content
+    allMultilingualBlocks = $(".ml");
+    this.update( allMultilingualBlocks );
+}
 
-        if( animated && false ) {
-            former.fadeOut(400, function() {
-                former.removeClass("ml-on").addClass("ml-off");
-                latter.fadeIn(400, function() {
-                    latter.removeClass("ml-off").addClass("ml-on");
-                });
-            });
-        } else {
+Multilingual.prototype.update = function( mlBlocks ) {
+    // Pass if no content
+    if( mlBlocks.length == 0 )
+        return;
+
+    if( typeof animated === 'undefined' )
+        animated = false;
+
+    former = mlBlocks.find(".ml-on");
+    latter = mlBlocks.mlFindLocal( this.activeLanguage );
+
+    if( animated && false ) {
+        former.fadeOut(400, function() {
             former.removeClass("ml-on").addClass("ml-off");
-            latter.removeClass("ml-off").addClass("ml-on");
-        }
-
+            latter.fadeIn(400, function() {
+                latter.removeClass("ml-off").addClass("ml-on");
+            });
+        });
+    } else {
+        former.removeClass("ml-on").addClass("ml-off");
+        latter.removeClass("ml-off").addClass("ml-on");
     }
 }
 
-// Attach click-handlers to each ml language button
-mlWidget.find(".ml-language").click( function() {
-    if( $(this).hasClass("selected") )
-        return;
-    setLanguage( $(this).attr("lang"), true );
+
+Multilingual.prototype.initialise = function() {
+    this.widgets = $(".ml-widget");
+
+    var mlthis = this;
+    // Attach click-handlers to each ml language button
+    this.widgets.find(".ml-language").click( function() {
+        if( $(this).hasClass("active") )
+            return;
+        mlthis.activate( $(this).attr("lang"), true );
+    });
+
+    // Check for a previously selected language
+    savedLang = this.cookied();
+    if( !savedLang ) {
+        // Default to Japanese
+        savedLang = "ja";
+    }
+    langButton = this.button( savedLang );
+
+    // We take the presence of a button for a particular language
+    // to mean that this website has that language available.
+    // Therefore, only set to that language if it's available.
+    if( langButton ) {
+        this.activate( savedLang, false );
+    }
+}
+
+
+var multilingual;
+$(document).ready( function() {
+    multilingual = new Multilingual();
+    multilingual.initialise();
 });
 
-
-priorSelection = $.cookie("ml-language-selection");
-if( !priorSelection ) {
-    priorSelection = "ja";
-}
-langButton = mlButton( priorSelection );
-
-// We take the presence of a button for a particular language
-// to mean that this website has that language available.
-// Therefore, only set to that language if it's available.
-if( langButton ) {
-    setLanguage( priorSelection, false );
-}
