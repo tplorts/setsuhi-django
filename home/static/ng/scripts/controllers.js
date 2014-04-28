4aa97aa998e5c027efec90ad659677ca90558cb7
Ted Lorts <tplorts@gmail.com>
2014-04-29 05:36:38 +0900

    achieved some simple swapping in the the workroom

--- home/static/ng/scripts/controllers.js
+++ home/static/ng/scripts/controllers.js
@@ -2,7 +2,27 @@
 
 /* Controllers */
 
-angular.module('workroomApp.controllers', [])
-    .controller('PictureListController', function($scope) {
+var cmod = angular.module('workroomApp.controllers', []);
+
+cmod.controller(
+    'PictureListController', 
+    ['$scope', function($scope) {
         $scope.pictureEntries = ngData_pictureEntries;
-    });
+        var entries = $scope.pictureEntries;
+        for( var i=0; i < entries.length; i++ )
+            entries[i]['arrayIndex'] = i;
+
+        $scope.moveEntry = function( fromIndex, toIndex ) {
+            if( fromIndex != toIndex ) {
+                console.log('move from ' + fromIndex + ' to ' + toIndex);
+                $scope.$apply(function() {
+                    var eFrom = entries[fromIndex];
+                    var eTo = entries[toIndex];
+                    var oi = eFrom.fields.order_index;
+                    eFrom.fields.order_index = eTo.fields.order_index;
+                    eTo.fields.order_index = oi;
+                });
+            }
+        };
+    }]
+);
--- home/static/ng/scripts/directives.js
+++ home/static/ng/scripts/directives.js
@@ -12,25 +12,83 @@ dmod.directive('appVersion', ['version', function(version) {
     };
 }]);
 
+var priorEntrance = null;
 
 dmod.directive('tawReorderableEntry', ['$rootScope', function($rootScope) {
     function link(scope, el, attrs, controller) {
-        angular.element(el).attr("draggable", "true");
-        
-        var index = scope.$index;
-        var id = attrs.id;
+        var ngel = angular.element(el);
+        ngel.attr("draggable", "true");
 
-        el.bind("dragstart", function(e) {
-            console.log("dstart ["+index+'] #'+id);
+        var thisIndex = attrs.index;
+
+        el.bind("dragstart", function(eve) {
+            var e = eve.originalEvent;
+            var dt = e.dataTransfer;
+            ngel.addClass('dragged-from');
+            dt.effectAllowed = 'move';
+            dt.setData("text/plain", thisIndex);
+        });
+
+        el.bind("dragend", function(e) {
+            ngel.removeClass('dragged-from');
+            priorEntrance = null;
+        });
+
+
+
+        el.bind("dragover", function(eve) {
+            var e = eve.originalEvent;
+            var dt = e.dataTransfer;
+            if (e.preventDefault) {
+                // Necessary. Allows us to drop.
+                e.preventDefault();
+            }
+            dt.dropEffect = 'move';
+            return false;
+        });
+
+        el.bind("dragenter", function(eve) {
+            var e = eve.originalEvent;
+            var dt = e.dataTransfer;
+            if( thisIndex != priorEntrance ) {
+                priorEntrance = thisIndex;
+                var sourceIndex = dt.getData('text/plain');
+                if( sourceIndex != thisIndex ) {
+                    ngel.addClass('dragged-over');
+                }
+            }
         });
         
-        el.bind("dragend", function(e) {
-            console.log('end');
+        el.bind("dragleave", function(eve) {
+            var e = eve.originalEvent;
+            ngel.removeClass('dragged-over');
         });
+
+        el.bind("drop", function(eve) {
+            var e = eve.originalEvent;
+            var dt = e.dataTransfer;
+            if (e.stopPropagation) {
+                // Stops some browsers from redirecting.
+                e.stopPropagation();
+            }
+            ngel.removeClass('dragged-over');
+            var oldIndex = dt.getData("text/plain");
+            if( thisIndex != oldIndex ) {
+                scope.onDrop({
+                    fromIndex: oldIndex, 
+                    toIndex: thisIndex
+                });
+            }
+            return false;
+        });
+
     }
 
     return {
         restrict: 'A',
-        link: link
+        link: link,
+        scope: {
+            onDrop: '&'
+        }
     }
 }]);
